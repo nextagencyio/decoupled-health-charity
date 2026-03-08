3be73e6080012df1d4de9fe3eacaad6639f887ac
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/campaigns/page.tsx
+++ app/campaigns/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import CampaignCard from '../components/CampaignCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Campaigns | Wellspring Health Foundation',
--- app/research/page.tsx
+++ app/research/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ResearchProjectCard from '../components/ResearchProjectCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Research | Wellspring Health Foundation',
